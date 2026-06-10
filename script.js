9c1b0ae4c6e9591f3bf1254fc417ff88c114a3d5
Alex Bond <countwessex@gmail.com>
2026-06-10 17:28:21 +0500

    ver 1

---
+++ script.js
@@ -0,0 +1,15 @@
+if (window.lucide) {
+  window.lucide.createIcons();
+}
+
+document.querySelectorAll('a[href^="#"]').forEach((link) => {
+  link.addEventListener("click", (event) => {
+    const target = document.querySelector(link.getAttribute("href"));
+    if (!target) {
+      return;
+    }
+
+    event.preventDefault();
+    target.scrollIntoView({ behavior: "smooth", block: "start" });
+  });
+});
